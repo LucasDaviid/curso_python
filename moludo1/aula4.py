e229432b3c0cd7816f57ea541aa4e3ecc70dfac5
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 17:07:51 -0300

    Aula 4 - int, float, type

---
+++ moludo1/aula4.py
@@ -0,0 +1,16 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número positivo ou negativo.
+# int sem sinal é considerado positivo.
+
+print(11, -11, 0) # int
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número positivo ou negativo.
+# float sem sinal é considerado positivo.
+
+print(1.7, -25.5, 0.0) # float
+
+# type retorna o tipo que o Python inferiu ao valor.
+
+print( type('Lucas'), type(25), type(-0.7) )

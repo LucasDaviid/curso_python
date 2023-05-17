4731982bac909bda0c86516d869820f769d3454e
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 17:17:27 -0300

    Aula 5 - boolean

---
+++ moludo1/aula5.py
@@ -0,0 +1,7 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa só existem 2 respostas possíveis: True or False.
+# Existem diversos operadores para "questionar".
+# um deles é o ==, um operador lógico que questiona se um valor é = a outro.
+
+print(10 == 10) # True
+print(15 == 10) # False

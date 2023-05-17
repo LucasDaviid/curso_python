ac823f82908ee2e4bd418dacd411250e17ec1e13
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 18:21:52 -0300

    Aula 9 - Operadores aritiméticos

---
+++ moludo1/aula9.py
@@ -0,0 +1,22 @@
+# Operadores aritiméticos
+
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multipicacao = 10 * 10
+print('Multiplicação', multipicacao)
+
+divisao = 10 / 2.2  # Retorna um float 
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2  # Não retorna as casas decimais
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # Resto da divisão
+print('Módulo', modulo)

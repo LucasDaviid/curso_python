76f700c17df8a9e7f1ca1c19ee87f00beb921cdb
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 18:07:50 -0300

    Aula 8 - Exercício

---
+++ moludo1/aula8.py
@@ -0,0 +1,15 @@
+# Aula de exercício
+
+nome = 'Lucas'
+sobrenome = 'David'
+idade = 25
+ano_nascimento = 1997
+maior_idade = idade >= 18
+altura_metros = 1.76
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento )
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)

3f1398394d1cde753b7935a330599147646c6464
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 18:45:56 -0300

    Aula 12 - Exercício calculo IMC e Ellipsis

---
+++ moludo1/aula12.py
@@ -0,0 +1,11 @@
+# Exercício de programação - Cálculo do IMC (Índice de Massa Corpórea) + Ellipsis
+
+nome = 'Lucas David'
+altura = 1.76
+peso = 70
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura,', 
+      'pesa', peso, 'Kg e seu IMC é', imc)
+
+# ... é chamado de ellipsis que é um código que ainda não foi escrito e ele não gera erro no código

82c7ebd888b54dae8fff42ce360b7862998d4393
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 17:58:07 -0300

    Aula 7 - Variáveis

---
+++ moludo1/aula7.py
@@ -0,0 +1,9 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP 8(guia de estilo do cod. Python): Inicie variáveis com letras minúsculas, pode usar número e _
+# = é o operador de atribuição. É usado para atribuir um valor a uma variável
+# Uso: nome_variavel = expressão
+
+nome = 'Lucas David'
+idade = 25
+maior_idade = idade >= 18
+print('Nome:', nome, '\nIdade:', idade, 'anos', '\nMaior de idade?', maior_idade)

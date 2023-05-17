18cc619ea95647a5a2b39e7d039d86199e4e32f2
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 16:50:07 -0300

    Aula 3 - String, aspas, escape, r

---
+++ moludo1/aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipagem = Dinâmica / Forte
+str -> String -> texto
+"""
+
+# Aspas simples
+print('Lucas David')
+
+# Aspas duplas
+print("Lucas David")
+
+# Escape
+print("Lucas \"David\"")  # \ Ignora o caractere a frente
+
+print('Lucas "David"')  # Podemos alternar as aspas para que sejam exibidas sem a utilização de escape
+print("Lucas 'David'")  
+
+# r
+print(r"Lucas \"David\"")  # r exibe os caracteres de escape, utilizado para expressões regulares

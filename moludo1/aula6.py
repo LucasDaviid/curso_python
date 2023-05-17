7679bfef4330281cbf9b04a156d73ec801c4d775
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 17:38:10 -0300

    Aula 6 - typecasting

---
+++ moludo1/aula6.py
@@ -0,0 +1,6 @@
+# Conversão de tipos, coerção, type convertion, typecasting, coercion 
+# é o ato de converter um tipo em outro.
+# Tipos imutáveis e primitivos: str, int, float, bool
+
+print(int('1'), type(int('1'))) # typecasting de str para int
+print(float('1') + 1) # typecasting e soma

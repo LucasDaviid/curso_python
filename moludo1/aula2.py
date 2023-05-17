e4d919ac75a8ba42d2c95ea326e395c8b7683ae3
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-17 16:25:29 -0300

    Aula 2 - print, sep, end

---
+++ moludo1/aula2.py
@@ -0,0 +1,13 @@
+#Explicação da função Print
+
+"""
+\r \n --> CRLF
+\n --> LF
+"""
+print(1234, 567, sep='-') #sep é para setar qual separador você quer utilizar na linha
+print(89, 0, sep=" -tereza- ") 
+print(11, 12, end="##") #end é para setar o que ocorre no final da linha
+print(13, end=" - end \n" )
+print(14)
+
+#Python é Case-sensitive

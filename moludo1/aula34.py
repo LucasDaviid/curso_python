cd5ae0c917d802484b5a9d2cfc1b043458c1c4d8
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-20 16:47:34 -0300

    Aula 34 - Introdução ao for

---
+++ moludo1/aula34.py
@@ -0,0 +1,15 @@
+""" Introdução ao for / in 
+"""
+
+# texto = 'Python'
+
+# for letra in texto:
+#     print(letra)
+
+""" for + range(start, stop, step)
+"""
+
+# numeros = range(5, 10, 2)
+
+# for numero in numeros:
+#     print(numero)

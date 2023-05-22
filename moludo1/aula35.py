11373098713c38f63407cc7214015367993e0617
LucasDaviid <lucas_c_david@hotmail.com>
2023-05-22 14:40:57 -0300

    Aula - 35 Exercício - Jogo da palavra secreta

---
+++ moludo1/aula35.py
@@ -0,0 +1,49 @@
+# Exercício - Jogo da palavra secreta
+"""
+Faça um jogo para o usuário adivinhar qual
+a palavra secreta.
+- Você vai propor uma palavra secreta
+qualquer e vai dar a possibilidade para
+o usuário digitar apenas uma letra.
+- Quando o usuário digitar uma letra, você 
+vai conferir se a letra digitada está
+na palavra secreta.
+    - Se a letra digitada estiver na
+    palavra secreta; exiba a letra;
+    - Se a letra digitada não estiver
+    na palavra secreta; exiba *.
+Faça a contagem de tentativas do seu
+usuário.
+"""
+import os
+
+palavra = 'pedrada'
+letras_acertadas = ''
+tentativas = 0
+
+while True:   
+    letra_dig = input('Digite uma letra: ')
+    tentativas += 1
+
+    if len(letra_dig) > 1:
+        print('Digite apenas uma letra')
+        continue
+        
+    if letra_dig in palavra:
+        letras_acertadas += letra_dig
+
+    palavra_formada = ''
+    for letra_secreta in palavra:
+        if letra_secreta in letras_acertadas:
+           palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
+    print('Palavra formada: ', palavra_formada)
+
+    if palavra_formada == palavra:
+        os.system('cls') # limpa o terminal
+        print('Parabéns você ganhou!')
+        print('A palavra era:', palavra)
+        print('Tentativas:', tentativas)
+        break
+        

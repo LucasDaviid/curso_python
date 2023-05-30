91420dda250ec33865a2811e556a36d14f0344a3
LucasDaviid <Lucass_David@outlook.com>
2023-05-30 16:20:20 -0300

    Aula 30 - try, except e mais

--- modulo2/aula30.py
+++ modulo2/aula30.py
@@ -1,1 +1,18 @@
-# Teste
+#  try, except, else e finally + Built-in Exceptions
+# https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
+
+try:
+    print('ABRIR ARQUIVO')
+    8/0
+except ZeroDivisionError as e:
+    print(e.__class__.__name__) # Exibe o nome do erro
+    print(e) # Exibe a mensagem do erro
+    print('DIVIDIU ZERO')
+except IndexError as error:  # Captura o erro com as e uma variavel
+    print('IndexError')
+except (NameError, ImportError):
+    print('NameError, ImportError')
+else:
+    print('Não deu erro')
+finally:
+    print('FECHAR ARQUIVO')

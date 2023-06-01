b2f5777cf1363ce040ed5a5e7be7223151a5f8b3
LucasDaviid <Lucass_David@outlook.com>
2023-06-01 18:09:04 -0300

    Aula 34 - Proposta de 3 exercícios em 1

--- modulo2/aula34.py
+++ modulo2/aula34.py
@@ -1,25 +1,38 @@
 # Proposta de 3 exercícios em 1
 import copy
 from aula34_dados import produtos
-"""
- Ordene os produtos por nome decrescente (do maior para menor)
- Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
- Ordene os produtos por preco crescente (do menor para maior)
- Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
-"""
 #  Aumente os preços dos produtos a seguir em 10%
 #  Gere novos_produtos por deep copy (cópia profunda)
-
 
 novos_produtos= [
     {**p, 'preco': round(p['preco'] * 1.1, 2)} # Recriamos a chave preço, subimos em 10% e round para 2 casas decimais
     for p in copy.deepcopy(produtos)
 ]
 
+#  Ordene os produtos por nome decrescente (do maior para menor)
+#  Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome'], # Função recebe o produto e retorna o nome
+    reverse=True
+)
+
+#  Ordene os produtos por preco crescente (do menor para maior)
+#  Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+produtos_ordenados_por_preco = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['preco'], # Função recebe o produto e retorna o preço
+)
+
+# Exibir
+
 print(*produtos, sep='\n') 
-print()
+print('\nPreço elevado em 10%:\n')
 print(*novos_produtos, sep='\n')
-
-
-
+print('\nProdutos ordenados por nome:\n')
+print(*produtos_ordenados_por_nome, sep='\n')
+print('\nProdutos ordenados por preço:\n')
+print(*produtos_ordenados_por_preco, sep='\n')

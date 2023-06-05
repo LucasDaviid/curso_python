2705b41bdf5b6ee30ad20422e1c1282c26768b19
LucasDaviid <Lucass_David@outlook.com>
2023-06-05 15:17:21 -0300

    Explicação da variável a ser livre

--- modulo2/aula36.py
+++ modulo2/aula36.py
@@ -1,2 +1,13 @@
 # Variáveis livres + nonlocal
+def fora(x):
+    a = x # Variavel a é livre pois não está definida dentro do escopo da função dentro()
 
+    def dentro():
+        return a
+    return dentro
+
+dentro1 = fora(10)
+dentro2 = fora(20)
+
+print(dentro1())
+print(dentro2())

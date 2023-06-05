5bbc25b4e787945dadd07111073a03a2f8f4cf5a
LucasDaviid <Lucass_David@outlook.com>
2023-06-05 17:06:18 -0300

    Aula 40 - Ordem dos decoradores

--- modulo2/aula40.py
+++ modulo2/aula40.py
@@ -6,12 +6,13 @@ def parametros_decorador(nome):
 
         def sua_nova_funcao(*args, **kwargs):
             res = func(*args, **kwargs)
-            final = f'{res} {nome}'
+            final = f'{res} {nome}' # Convertido o resultado da func de int/float para string passando uma fstring e concatenamos o nome do decorador
             return final
         return sua_nova_funcao
     return decorador
 
-@parametros_decorador(nome='primeiro')
+@parametros_decorador(nome='segundo')
+@parametros_decorador(nome='primeiro') # A ordem de aplicação dos decoradores é debaixo para cima
 def soma(x, y):
     return x + y
 

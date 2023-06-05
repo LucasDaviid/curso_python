6b7005668238814949dd144d09803aa63c2c742e
LucasDaviid <Lucass_David@outlook.com>
2023-06-05 15:26:36 -0300

    Função globals e verif. de variável livre

--- modulo2/aula36.py
+++ modulo2/aula36.py
@@ -1,11 +1,14 @@
 # Variáveis livres + nonlocal
 # funções locals e globals
 # locals - informa quais variáveis são locais
+# globals - informa as variáveis globais definidas
+
 def fora(x):
     a = x # Variável a é livre pois não está definida dentro do escopo da função dentro()
 
     def dentro():
-        print(locals())
+        # print(locals())
+        # print(dentro.__code__.co_freevars) # Podemos ver as variáveis livres com essa linha de cod.
         return a
     return dentro
 

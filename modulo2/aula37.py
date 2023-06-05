83b705173af752cb45c0d562359a57efe791fa98
LucasDaviid <Lucass_David@outlook.com>
2023-06-05 15:57:07 -0300

    Criando as funções

--- modulo2/aula37.py
+++ modulo2/aula37.py
@@ -4,9 +4,23 @@
  Funções decoradoras são funções que decoram outras funções.
  Decoradores são usados para fazer o Python usar as funções decoradoras em outras funções.
 """
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        for arg in args:
+            checar_string(arg)
+        resultado = func(*args, **kwargs)
+        #
+        return resultado()
+    return interna
 
 def inverte_string(string):
     return string[::-1]
 
-invertida = inverte_string('Lucas')
+def checar_string(param):
+    if not isinstance(param, str):
+        raise TypeError('param deve ser uma string')
+
+checagem_parametro = criar_funcao(inverte_string)
+
+invertida = inverte_string('123')
 print(invertida)

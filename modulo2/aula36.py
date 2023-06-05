c4b4bbc7a22e6d7392409c398aae898c309460dc
LucasDaviid <Lucass_David@outlook.com>
2023-06-05 15:37:43 -0300

    Explicação do UnboundLocalError

--- modulo2/aula36.py
+++ modulo2/aula36.py
@@ -1,19 +1,32 @@
 # Variáveis livres + nonlocal
-# funções locals e globals
-# locals - informa quais variáveis são locais
-# globals - informa as variáveis globais definidas
+"""
+ Funções locals e globals
+ locals - informa quais variáveis são locais
+ globals - informa as variáveis globais definidas
+"""
 
-def fora(x):
-    a = x # Variável a é livre pois não está definida dentro do escopo da função dentro()
+# def fora(x):
+#     a = x # Variável a é livre pois não está definida dentro do escopo da função dentro()
 
-    def dentro():
-        # print(locals())
-        # print(dentro.__code__.co_freevars) # Podemos ver as variáveis livres com essa linha de cod.
-        return a
-    return dentro
+#     def dentro():
+#         # print(locals())
+#         # print(dentro.__code__.co_freevars) # Podemos ver as variáveis livres com essa linha de cod.
+#         return a
+#     return dentro
 
-dentro1 = fora(10)
-dentro2 = fora(20)
+# dentro1 = fora(10)
+# dentro2 = fora(20)
 
-print(dentro1())
-print(dentro2())
+# print(dentro1())
+# print(dentro2())
+
+def concatenar(string_inicial):
+    valor_final = string_inicial
+    
+    def interna(valor_a_concatenar):
+        valor_final += valor_a_concatenar # Vai gerar um UnboundLocalError. Precisamos informar que a variável não é local
+        return valor_final
+    return interna
+
+c = concatenar('a')
+print(c('b'))

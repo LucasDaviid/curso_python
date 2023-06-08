36c613af256ad5435963339810ee407e2681fa99
LucasDaviid <Lucass_David@outlook.com>
2023-06-08 17:23:56 -0300

    Exemplificação de funcionamento

--- modulo2/aula45.py
+++ modulo2/aula45.py
@@ -1,1 +1,24 @@
 # groupby - agrupando valores (itertools)
+# Em groupby os dados precisam estar ordenados para que ele possa agrupar corretamente
+# 
+from itertools import groupby
+
+alunos = [
+    {'nome': 'Lucas', 'nota': 'A'},
+    {'nome': 'Sei', 'nota': 'B'},
+    {'nome': 'Bia', 'nota': 'A'},
+    {'nome': 'Pedro', 'nota': 'C'},
+    {'nome': 'Matheus', 'nota': 'D'},
+    {'nome': 'Ciel', 'nota': 'A'},
+    {'nome': 'Nara', 'nota': 'B'},
+    {'nome': 'Igor', 'nota': 'A'},
+    {'nome': 'Stephany', 'nota': 'C'},
+]
+
+# Exemplificação de funcionamento
+alunos = ['a', 'a', 'a', 'b', 'c', 'a'] # Aqui tera 2 grupos de 'a', pois um deles não está ordenado
+grupos = groupby(sorted(alunos)) # Utilizamos sorted para ordenar a lista e resolver o problema acima.
+
+for chave, grupo in grupos:
+    print(chave)
+    print(list(grupo))

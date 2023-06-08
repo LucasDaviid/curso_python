79e81f56c844d6e82aa14671aac7ae5fff1f24e5
LucasDaviid <Lucass_David@outlook.com>
2023-06-08 16:06:59 -0300

    Diferenças entre count e range

--- modulo2/aula43.py
+++ modulo2/aula43.py
@@ -1,8 +1,8 @@
 # count é um iterador sem fim (itertools)
 from itertools import count
 
-c1 = count()
-r1 = range(10)
+c1 = count(8, 8) # Aqui passamos o início e o step
+r1 = range(8, 50, 8) # Aqui passamos o início fim e o step
 
 # O count entrega o valor e soma +1
 
@@ -11,3 +11,13 @@ print('c1', hasattr(c1, '__next__')) # Checando se o objeto é um iterator
 
 print('r1', hasattr(r1, '__iter__')) 
 print('r1', hasattr(r1, '__next__'))
+
+print('\ncount')
+for i in c1:
+    if i > 50: # Para evitar o loop infinito
+        break
+    print(i)
+
+print('\nrange')
+for i in r1:
+    print(i)

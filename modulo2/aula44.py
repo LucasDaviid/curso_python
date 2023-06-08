6a2304561b034490951448d209bcd1736dfd1eaf
LucasDaviid <Lucass_David@outlook.com>
2023-06-08 16:22:46 -0300

    Sobre combinations

--- modulo2/aula44.py
+++ modulo2/aula44.py
@@ -1,1 +1,16 @@
 #  Combinations, Permutations e Product - Itertools
+"""
+ Combinations - Ordem não importa - iterável + tamanho do grupo
+ Permutations - Ordem importa
+ Product - Ordem importa e repete valores únicos
+"""
+from itertools import combinations
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+pessoas = ['João', 'Joana', 'Luiz', 'Letícia',]
+camisetas = [['preta', 'branca'],]
+
+print_iter(combinations(pessoas, 2)) # Combinação das pessoas em grupos de 2

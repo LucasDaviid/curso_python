85c31bc9218d7725970da97f13b54b7206334720
LucasDaviid <Lucass_David@outlook.com>
2023-06-08 16:30:06 -0300

    Sobre product

--- modulo2/aula44.py
+++ modulo2/aula44.py
@@ -4,14 +4,19 @@
  Permutations - Ordem importa
  Product - Ordem importa e repete valores únicos
 """
-from itertools import combinations, permutations
+from itertools import combinations, permutations, product
 
 def print_iter(iterator):
     print(*list(iterator), sep='\n')
     print()
 
 pessoas = ['João', 'Joana', 'Luiz', 'Letícia',]
-camisetas = [['preta', 'branca'],]
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g', 'xg'],
+]
 
-print_iter(combinations(pessoas, 2)) # Combinação das pessoas em grupos
-print_iter(permutations(pessoas, 2)) # Permutação das pessoas em grupos
+# print_iter(combinations(pessoas, 2)) # Combinação das pessoas em grupos
+# print_iter(permutations(pessoas, 2)) # Permutação das pessoas em grupos
+print_iter(product(*camisetas))
+

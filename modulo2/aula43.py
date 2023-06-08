adbb0ce9e22439f5a2d763ec7e2fc0e2209d1af6
LucasDaviid <Lucass_David@outlook.com>
2023-06-08 15:58:07 -0300

    Checando se o objeto é um interavel e um iterator

--- modulo2/aula43.py
+++ modulo2/aula43.py
@@ -1,1 +1,13 @@
 # count é um iterador sem fim (itertools)
+from itertools import count
+
+c1 = count()
+r1 = range(10)
+
+# O count entrega o valor e soma +1
+
+print('c1', hasattr(c1, '__iter__')) # Checando se o objeto é um interavel
+print('c1', hasattr(c1, '__next__')) # Checando se o objeto é um iterator
+
+print('r1', hasattr(r1, '__iter__')) 
+print('r1', hasattr(r1, '__next__'))

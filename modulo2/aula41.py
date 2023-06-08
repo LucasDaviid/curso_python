154038dfab26ae0fd7ebd0ca4e627048977f645f
LucasDaviid <Lucass_David@outlook.com>
2023-06-08 14:45:05 -0300

    Função zip

--- modulo2/aula41.py
+++ modulo2/aula41.py
@@ -10,11 +10,12 @@ Use todos os valores da menor lista.
  [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
  """
 
-def zipper(lista1,lista2):
-    intervalo_maximo = min(len(lista1), len(lista2))
-    return [(lista1[i], lista2[i]) for i in range(intervalo_maximo)]
+# def zipper(lista1,lista2):
+#     intervalo_maximo = min(len(lista1), len(lista2))
+#     return [(lista1[i], lista2[i]) for i in range(intervalo_maximo)]
 
 
 l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
 l2 = ['BA', 'SP', 'MG', 'RJ']
-print(zipper(l1, l2))    
+print(list(zip(l1, l2)))     # A função zip faz a mesma coisa mas retorna um interaitor.
+# Para visualizar o interaitor podemos consumi-lo com uma list ou usar um for

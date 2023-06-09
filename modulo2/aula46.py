db2d9943b1e375c2c41f68a544914d1fae454cdd
LucasDaviid <Lucass_David@outlook.com>
2023-06-09 14:37:36 -0300

    declarada lista de produtos

--- modulo2/aula46.py
+++ modulo2/aula46.py
@@ -1,1 +1,16 @@
 #   map, partial, GeneratorType e esgotamento de Iterators
+
+def print_inter(interator):
+    print(*list(interator), sep='\n')
+    print()
+
+
+produtos = [
+    {'nome': 'produto 5', 'preco': 10.00},
+    {'nome': 'produto 1', 'preco': 22.32},
+    {'nome': 'produto 3', 'preco': 10.11},
+    {'nome': 'produto 2', 'preco': 105.87},
+    {'nome': 'produto 4', 'preco': 69.90},
+]
+
+print_inter(produtos)

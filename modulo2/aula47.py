7db237c95145805518dd00b4f243a92bcf360ed6
LucasDaviid <Lucass_David@outlook.com>
2023-06-14 15:50:32 -0300

    Código inicial

--- modulo2/aula47.py
+++ modulo2/aula47.py
@@ -1,1 +1,16 @@
 # filter é um filtro funcional
+
+def print_inter(iterador):
+    print(*list(iterador), sep='\n')
+    print()
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+
+print_inter(produtos)
